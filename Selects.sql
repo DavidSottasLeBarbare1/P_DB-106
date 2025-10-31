2898844ec93f961b8db432eae8590b73a68be0d1
Fnekk <pk88sdk@eduvaud.ch>
2025-10-31 22:50:40 +0100

    finished the 10 select requests

--- Selects.sql
+++ Selects.sql
@@ -31,4 +31,44 @@ GROUP BY npa, localite
 ORDER BY npa;
 
 /*Requête 5*/
+SELECT HOUR(c.date_creation) AS "heure", COUNT(*) AS "nbrCommande"
+FROM t_commande AS c
+GROUP BY HOUR(heure)
+ORDER BY nbrCommande DESC;
+
+/*Requête 6*/
+SELECT cl.nom AS "nom",cl.prenom AS "prenom", COUNT(c.commande_id) AS "nbrCommande"
+FROM t_commande AS c
+JOIN t_client AS cl ON c.client_fk = cl.client_id
+GROUP BY cl.client_id, nom, prenom
+HAVING nbrCommande >= 5
+ORDER BY nbrCommande DESC, nom ASC;
+
+/*Requête 7*/
+SELECT c.commande_id AS "commande", ROUND(SUM(lc.quantite * lc.prix_unitaire), 2) AS "montant"
+FROM t_commande AS c
+JOIN t_ligne_commande AS lc ON commande = lc.commande_fk
+GROUP BY commande
+ORDER BY commande ASC;
+
+/*Requête 8*/
+SELECT c.commande_id AS "commande", ROUND(SUM(p.montant), 2) AS "montant"
+FROM t_commande AS c
+JOIN t_paiement AS p ON p.commande_fk = commande
+GROUP BY commande
+ORDER BY commande ASC;
+
+/*Requête 9*/
+SELECT c.type AS "type", COUNT(*) AS "nbrCommande"
+FROM t_commande AS c
+GROUP BY type
+ORDER BY nbrCommande DESC;
+
+/*Requête 10*/
+SELECT l.livreur_id AS "livreur", l.nom AS "nom", ROUND(AVG(TIMESTAMPDIFF(MINUTE, liv.date_depart, liv.date_arrivee)), 2) AS "delaiMoyen"
+FROM t_livraison AS liv
+JOIN t_livreur AS l ON liv.livreur_fk = livreur
+WHERE liv.date_depart IS NOT NULL AND liv.date_arrivee IS NOT NULL
+GROUP BY livreur, nom
+ORDER BY delaiMoyen ASC;
 
